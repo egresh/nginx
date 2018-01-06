cb8f28d03ca8397efeafdb64099160b63af87ffd
Earl Gresh <egresh@qualcomm.com>
2018-01-05 17:54:17 -0800

    Virtual Host Configuration Location
    All virtual hosts should now be placed in /etc/nginx/conf.d

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,14 @@
 # Enginx Cookbook Changelog
 
-# 0.1.0
+# 1.0.0
 
-- Initial Development
+BREAKING CHANGES:
+- Web site configuration must be in /etc/nginx/conf.d
 
 # 0.1.1
 
 - Integrating with Travis CI
+
+# 0.1.0
+
+- Initial Development
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,3 +20,20 @@ template '/var/www/html/index.nginx-debian.html' do
   group 'root'
   mode '0644'
 end
+
+template '/etc/nginx/nginx.conf' do
+  source 'nginx.conf.erb'
+  notifies :restart, 'service[nginx]', :immediately
+end
+
+directory '/etc/nginx/conf.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+end
+
+template '/etc/nginx/conf.d/default.conf' do
+  source 'default.conf.erb'
+  notifies :restart, 'service[nginx]', :immediately
+end
